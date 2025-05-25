3e6f9f7ce1899c9f8641ba9c1b370082b424b6f1
JILM1979 <entradajuan@gmail.com>
2025-05-25 19:34:25 +0200

    cambios de BD para railway

--- src/db.js
+++ src/db.js
@@ -1,12 +1,20 @@
 import pkg from 'pg';
 const { Pool } = pkg;
-
+/*
 const pool = new Pool({
   user: 'postgres',
   host: 'db',
   database: 'articulosdb',
   password: 'postgres',
   port: 5432,
+});
+*/
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
 });
 
 export default pool;
